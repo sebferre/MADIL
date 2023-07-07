9df9fa576f2c3a4b1ea65496b343cd13b7ae91d3
Sebastien Ferre <ferre@irisa.fr>
2023-07-07 11:56:39 +0200

    [model] compile returns non-det programs + encoder

--- src/model.ml
+++ src/model.ml
@@ -38,32 +38,45 @@ let xp_model
 
 
 let compile
-      ~(x_constr : 'a -> 'constr -> 'a) (* input preparation for constr *)
+      ~(x_constr : 'a -> 'constr -> 'a Myseq.t) (* input preparation for constr *)
       ~(x_field : 'a -> 'constr -> int -> 'a) (* dispatch pattern input to each field *)
-      ~(y_constr : 'a -> 'constr -> 'b array -> 'b) (* pattern output from input, and argument outputs *)
-      ~(x_first : 'a -> 'a) (* initial input for sequence *)
-      ~(x_next : 'a -> 'b -> 'a) (* next input in sequence, from previous item input and output *)
-      ~(y_seq : 'a -> 'b list -> 'a -> 'b) (* sequence output from input, item outputs, and remainder input *)
-      (m : ('constr,'func) model) : 'a -> 'b =
-  let rec aux : ('constr,'field) model -> ('a -> 'b) = function
+      ~(y_constr : 'a -> 'constr -> 'b array -> 'b Myseq.t) (* pattern output from input, and argument outputs *)
+      ~(x_first : 'a -> 'a Myseq.t) (* initial input for sequence *)
+      ~(x_next : 'a -> 'b -> 'a Myseq.t) (* next input in sequence, from previous item input and output *)
+      ~(y_seq : 'a -> 'b list -> 'a -> 'b Myseq.t) (* sequence output from input, item outputs, and remainder input *)
+      (m : ('constr,'func) model) : 'a -> 'b Myseq.t=
+  let rec aux : ('constr,'field) model -> ('a -> 'b Myseq.t) = function
     | Pat (c,args) ->
        let g_args = Array.map aux args in
        (fun x ->
-         let x = x_constr x c in
-         let y_args = Array.mapi (fun i g_arg -> g_arg (x_field x c i)) g_args in
-         y_constr x c y_args)
+         let* x = x_constr x c in
+         let seq_args = Array.mapi (fun i g_arg -> g_arg (x_field x c i)) g_args in
+         let* l_args = Myseq.product_fair (Array.to_list seq_args) in
+         let args = Array.of_list l_args in
+         y_constr x c args)
     | Expr e -> assert false
     | Seq (n,lm1) ->
-       let g_lm1 = List.map aux lm1 in
+       let g_lm1 =
+         List.map
+           (fun mi ->
+             let gi = aux mi in
+             (fun xi ->
+               let* yi = gi xi in
+               let* x_next = x_next xi yi in
+               Myseq.return (yi,x_next)))
+           lm1 in
+       (*let g_lm1 = List.map aux lm1 in*)
        (fun x ->
-         let x1 = x_first x in
-         let xN, rev_ly =
+         let* x1 = x_first x in
+         let* ly, xN = Myseq.product_dependent_fair g_lm1 x1 in
+(*         let xN, rev_ly =
            List.fold_left
              (fun (xi, rev_ly) gi ->
                let yi = gi xi in
                x_next xi yi, yi::rev_ly)
              (x1,[]) g_lm1 in
-         y_seq x (List.rev rev_ly) xN)
+         let ly = List.rev rev_ly in *)
+         y_seq x ly xN)
     | Cst m1 -> raise TODO
 (*       let g_m1 = aux m1 in
        (fun x ->
@@ -72,35 +85,75 @@ let compile
   in
   aux m
 
+type ('value,'dconstr) generator = unit -> ('value,'dconstr) data Myseq.t
+  
 let generator (* naive *)
       ~(output_constr : 'constr -> ('value,'dconstr) data array -> ('value,'dconstr) data)
-    : ('constr,'func) model -> (unit -> ('value, 'constr) data) =
+    : ('constr,'func) model -> ('value, 'dconstr) generator =
   compile
-    ~x_constr:(fun () c -> ())
+    ~x_constr:(fun () c -> Myseq.return ())
     ~x_field:(fun () c i -> ())
-    ~y_constr:(fun () c args -> output_constr c args)
-    ~x_first:(fun () -> ())
-    ~x_next:(fun () _ -> ())
-    ~y_seq:(fun () ld () -> DSeq (List.length ld, ld))
+    ~y_constr:(fun () c args -> Myseq.return (output_constr c args))
+    ~x_first:(fun () -> Myseq.return ())
+    ~x_next:(fun () _ -> Myseq.return ())
+    ~y_seq:(fun () ld () -> Myseq.return (DSeq (List.length ld, ld)))
 
+
+type ('input,'value,'dconstr) parseur = 'input -> ('value,'dconstr) data Myseq.t
+  
 let parseur
-      ~(input_constr : 'input -> 'constr -> 'input)
+      ~(input_constr : 'input -> 'constr -> 'input Myseq.t)
       ~(input_field : 'input -> 'constr -> int -> 'input)
       ~(output_constr : 'input -> 'constr -> ('value,'dconstr) data array -> ('value,'dconstr) data Myseq.t)
-      ~(input_first : 'input -> 'input)
-      ~(input_next : 'input -> ('value,'dconstr) data Myseq.t -> 'input)
-      ~(output_seq : 'input -> int -> ('value,'dconstr) data list -> 'input -> ('value,'dconstr) data Myseq.t)
-    : ('constr,'func) model -> ('input -> ('value,'dconstr) data Myseq.t) =
+      ~(input_first : 'input -> 'input Myseq.t)
+      ~(input_next : 'input -> ('value,'dconstr) data -> 'input Myseq.t)
+      ~(output_seq : 'input -> ('value,'dconstr) data list -> 'input -> ('value,'dconstr) data Myseq.t)
+    : ('constr,'func) model -> ('input,'value,'dconstr) parseur =
   compile
     ~x_constr:input_constr
     ~x_field:input_field
-    ~y_constr:(fun input c seq_args ->
-      let* l_args = Myseq.product_fair (Array.to_list seq_args) in
-      let args = Array.of_list l_args in
-      output_constr input c args)
+    ~y_constr:output_constr
     ~x_first:input_first
     ~x_next:input_next
-    ~y_seq:(fun input seq_ld input_last ->
-      let n = List.length seq_ld in
-      let* ld = Myseq.product_fair seq_ld in
-      output_seq input n ld input_last)
+    ~y_seq:output_seq
+
+
+type ('value,'dconstr) encoder = ('value,'dconstr) data -> dl
+
+let encoder
+      ~(dl_constr : 'constr -> 'dconstr -> dl)
+    : ('constr,'func) model -> ('value,'dconstr) encoder =
+  let aux : ('constr,'func) model -> (('value,'dconstr) data * ('value,'dconstr) data list -> dl Myseq.t) =
+    compile
+      ~x_constr:(fun (d,_) c -> Myseq.return (d,[]))
+      ~x_field:(fun (d,_) c i ->
+        match d with
+        | DVal (v, DPat (dc, args)) -> (args.(i), [])
+        | _ -> failwith "Model.encoder: unexpected data w.r.t. model")
+      ~y_constr:(fun (d,_) c dl_args ->
+        match d with
+        | DVal (v, DPat (dc, args)) ->
+           let dl_dc = dl_constr c dc in
+           let dl = Array.fold_left (fun res dl_arg -> res +. dl_arg) dl_dc dl_args in
+           Myseq.return dl
+        | _ -> assert false)
+      ~x_first:(fun (d,_) ->
+        match d with
+        | DSeq (_, d0::ld1) -> Myseq.return (d0,ld1)
+        | _ -> assert false)
+      ~x_next:(fun (di,ldi) _ ->
+        match ldi with
+        | [] -> Myseq.return (di,[]) (* should have some Null instead of di *)
+        | d0::ld1 -> Myseq.return (d0,ld1))
+      ~y_seq:(fun (d,_) dl_items (_,ldi_last) ->
+        assert (ldi_last = []);
+        (* no need to encode nb of items, rigid sequence model *)
+        let dl = List.fold_left (fun res dl_item -> res +. dl_item) 0. dl_items in
+        Myseq.return dl)
+  in
+  fun m ->
+  let g = aux m in
+  fun d ->
+  match Myseq.hd_opt (g (d,[])) with
+  | Some dl -> dl
+  | None -> assert false
