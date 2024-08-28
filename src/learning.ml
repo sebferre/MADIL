813a973389171a9d5d634715bfb0045de07fe860
Sebastien Ferre <ferre@irisa.fr>
2024-08-28 13:59:58 +0200

    [learning] abstraction of continuations to allow for customized ordering
    + taking into account completed models/states (final DL)

--- src/learning.ml
+++ src/learning.ml
@@ -29,14 +29,77 @@ type ('typ,'value,'var,'constr,'func) state =
     lrido : dl; (* input rank + output data normalized DL *)
   }
 
-let print_jumps jumps =
-  jumps
-  |> List.rev
-  |> List.iter
-       (function
-        | 0 -> print_string " -"
-        | rank -> print_char ' '; print_int rank)
-  
+(* encapsulation of state and search path information *)
+(* using an object to hide some mysterious functional value inside state that is a problem with Bintree *)
+class ['state] ostate (state : 'state) =
+  object (self)
+    val nsteps_sol : int = 0 (* nb of refinement steps from initial model *)
+    val jumps_sol : int list = [] (* reverse list of refinement ranks from initial model *)
+    val state : 'state = state (* reached state *)
+    method nsteps_sol = nsteps_sol
+    method jumps_sol = jumps_sol
+    method state = state
+
+    method child (rank : int) (state1 : 'state) =
+      (* building the child ostate following rank-th refinement *)
+      {< nsteps_sol = 1 + nsteps_sol;
+         jumps_sol = rank :: jumps_sol;
+         state = state1 >}
+
+    method print_jumps =
+      jumps_sol
+      |> List.rev
+      |> List.iter
+           (function
+            | 0 -> print_string " -"
+            | rank -> print_char ' '; print_int rank)
+  end
+
+class type ['state] conts =
+  (* mutable collection of search continuation points *)
+  object
+    method push : 'state (* final/completed state *)
+                  -> 'state ostate (* incomplete state as continuation point *)
+                  -> unit (* added to collection, using information from final state *)
+    method pop : 'state ostate (* most promising continuation point, removed from collection *)
+  end
+
+class virtual ['state,'fitness] conts_fitness =
+        (* fitness function to specify ordering of continuations *)
+  object (self)
+    val mutable conts : ('fitness * 'state ostate) Bintree.t = Bintree.empty
+    method virtual fitness : 'state -> 'state ostate -> 'fitness
+    method push (final_state : 'state) (ostate : 'state ostate) : unit =
+      let fitness = self#fitness final_state ostate in
+      conts <- Bintree.add (fitness, ostate) conts
+    method pop : 'state ostate =
+      let _, ostate = Bintree.min_elt conts in
+      conts <- Bintree.remove_min_elt conts;
+      ostate
+  end
+
+class ['state] conts_v0 =
+  object
+    constraint 'fitness = int * int
+    inherit ['state, 'fitness] conts_fitness
+    method fitness final_state ostate =
+      let _, njumps, sumdepths =
+        List.fold_right
+          (fun rank (depth, njumps, sumdepths) ->
+            (depth + 1, njumps + rank,
+             if rank > 0 then sumdepths + depth else sumdepths))
+          ostate#jumps_sol (0,0,0) in
+      (njumps, sumdepths)
+  end
+
+class ['state] conts_v1 =
+  object
+    constraint 'fitness = float * int * int
+    inherit ['state, 'fitness] conts_fitness
+    method fitness final_state ostate =
+      (final_state.lmd, ostate#nsteps_sol, List.hd ostate#jumps_sol)
+  end
+
 let learn
       ~(alpha : float)
       ~(read_pairs :
@@ -98,18 +161,17 @@ let learn
     match data_of_model ~pruning:false r0 m0 with
     | Some state0 -> state0
     | None -> failwith "Learning.learn: invalid initial task model" in
+  let ostate0 : _ ostate = new ostate state0 in
   let nsteps_ref = ref 0 in (* total nb steps *)
-  let nsteps_sol_ref = ref 0 in (* nb steps for solution path *)
   let njumps_ref = ref 0 in (* total nb jumps *)
-  let jumps_sol_ref = ref [] in (* jumps for solution path *)
-  let state_ref = ref state0 in
+  let ostate_sol_ref = ref ostate0 in (* current solution *)
+  let conts = new conts_v0 in (* collection of search continuations, mutable *)
   (* refining phase *)
-  let rec loop_refine nsteps_sol jumps_sol state delta conts =
+  let rec loop_refine ostate ostates = (* current ostate and list of pending continuation points *)
+    let state = ostate#state in
     (* recording current state as best state *)
-    if state.lrido = 0. || state.lmd < (!state_ref).lmd then (
-      nsteps_sol_ref := nsteps_sol;
-      jumps_sol_ref := jumps_sol;
-      state_ref := state );
+    if state.lrido = 0. || state.lmd < (!ostate_sol_ref)#state.lmd then (
+      ostate_sol_ref := ostate);
     log_refining state.r state.m state.prs state.lmd state.lrido;
     if state.lrido = 0. (* end of search *)
     then ()
@@ -128,20 +190,21 @@ let learn
                task_refinements state.m state.prs state.drsi state.drso)) in
       if lstate1 = [] (* no compressive refinement *)
       then (
-        try (* jumping to most promising continuation *)
-          let delta1, ostate1 = Bintree.min_elt conts in
-          let nsteps_sol1 = ostate1#nsteps_sol in
-          let jumps_sol1 = ostate1#jumps_sol in
-          let state1 = ostate1#state in
-          let conts = Bintree.remove_min_elt conts in
+        (* adding ostates as continuations *)
+        List.iter
+          (fun ostate -> conts#push state ostate)
+          ostates;
+        (* jumping to most promising continuation *)
+        try
+          let ostate1 = conts#pop in
           incr njumps_ref;
           let () = (* showing JUMP *)
             print_string "JUMP TO";
-            print_jumps jumps_sol1;
+            ostate1#print_jumps;
             print_newline () in
-          loop_refine nsteps_sol1 jumps_sol1 state1 delta1 conts
+          loop_refine ostate1 []
         with Not_found -> () ) (* no continuation *)
-      else
+      else (
         let lstate1 =
           List.sort
             (fun state1 state2 ->
@@ -150,10 +213,9 @@ let learn
         match lstate1 with
         | [] -> assert false
         | state1::others ->
-           let nsteps_sol1 = 1 + nsteps_sol in
-           let jumps_sol1 = 0::jumps_sol in
-           let nb_alts, conts =
-             List.fold_left
+           let ostate1 = ostate#child 0 state1 in
+           let nb_alts, ostates =
+             List.fold_left (* selecting alternate refinements, at most jump_width-1 *)
                (fun (rank,res) state2 ->
                  let ok =
                    rank < jump_width &&
@@ -164,48 +226,41 @@ let learn
                       | _ -> assert false) in
                  if ok
                  then
-                   let nsteps_sol2 = nsteps_sol + 1 in
-                   let jumps_sol2 = rank :: jumps_sol in
-                   let delta2 = fst delta + rank,
-                                snd delta + nsteps_sol2 in (* delta +. (lmd2 -. lmd1) *)
-                   let ostate2 =
-                     object
-                       method nsteps_sol = nsteps_sol2
-                       method jumps_sol = jumps_sol2
-                       method state = state2
-                     end in (* object to hide some mysterious functional value inside *)
-                   rank+1, Bintree.add (delta2, ostate2) res
+                   let ostate2 = ostate#child rank state2 in
+                   rank+1, ostate2::res
                  else rank, res)
-               (1,conts) others in
+               (1,ostates) others in
            incr nsteps_ref;
            let () = (* showing point of choice *)
              if nb_alts > 1 then (
                print_string "CHOICE AT";
-               print_jumps jumps_sol;
+               ostate#print_jumps;
                print_newline ()
              ) in
-           loop_refine nsteps_sol1 jumps_sol1 state1 delta conts
+           loop_refine ostate1 ostates)
   in
-  let state_refine, timed_out_refine, memed_out_refine =
+  let ostate_refine, timed_out_refine, memed_out_refine =
     print_endline "REFINING PHASE";
     let res_opt =
       Common.do_memout memout (* Mbytes *)
         (fun () ->
           Common.do_timeout_gc (float timeout_refine)
             (fun () ->
-              loop_refine 0 [] state0 (0,0) Bintree.empty)) in
-    !state_ref, (res_opt = Some None), (res_opt = None) in
+              loop_refine ostate0 [])) in
+    (!ostate_sol_ref), (res_opt = Some None), (res_opt = None) in
+  let state_refine = ostate_refine#state in
   let result_refining =
     { task_model = state_refine.m;
       pairs_reads = state_refine.prs;
       timed_out = timed_out_refine;
       memed_out = memed_out_refine;
       nsteps = (!nsteps_ref);
-      nsteps_sol = (!nsteps_sol_ref);
       njumps = (!njumps_ref);
-      njumps_sol = (List.fold_left (+) 0 !jumps_sol_ref);
+      nsteps_sol = (!ostate_sol_ref)#nsteps_sol;
+      njumps_sol = (List.fold_left (+) 0 (!ostate_sol_ref)#jumps_sol);
     } in
   (* pruning phase *)
+  let state_prune_ref = ref state_refine in
   let rec loop_prune state =
     log_refining state.r state.m state.prs state.lmd state.lrido;
     let lstate1 = (* computing the [refine_degree] most compressive valid refinements *)
@@ -229,8 +284,7 @@ let learn
     | [] -> ()
     | state1::_ ->
        incr nsteps_ref;
-       incr nsteps_sol_ref;
-       state_ref := state1; (* recording current state as best state *)           
+       state_prune_ref := state1; (* recording current state as best state *)
        loop_prune state1
   in
   let state_prune, timed_out_prune, memed_out_prune =
@@ -242,71 +296,21 @@ let learn
             (fun () ->
               let () =
                 match data_of_model ~pruning:true RInit state_refine.m with
-                | Some state0prune -> state_ref := state0prune
+                | Some state0prune ->
+                   state_prune_ref := state0prune
                 | None -> assert false in
-              loop_prune !state_ref)) in
-    !state_ref, (res_opt = Some None), (res_opt = None) in
+              loop_prune !state_prune_ref)) in
+    !state_prune_ref, (res_opt = Some None), (res_opt = None) in
   let result_pruning =
     { task_model = state_prune.m;
       pairs_reads = state_prune.prs;
       timed_out = timed_out_prune;
       memed_out = memed_out_prune;
       nsteps = (!nsteps_ref);
-      nsteps_sol = (!nsteps_sol_ref);
       njumps = (!njumps_ref);
-      njumps_sol = (List.fold_left (+) 0 !jumps_sol_ref);
+      nsteps_sol = result_refining.nsteps_sol;
+      njumps_sol = result_refining.njumps_sol;
     } in
   (* finalization *)
   { result_refining;
     result_pruning })
-    
-(* REM  
-  let lm_refine = [(state_refine.r, state_refine.m), state_refine, state_refine.lmd] in
-(*
-  let lm_refine, timed_out_refine =      
-    print_endline "REFINING PHASE";
-    Mdl.Strategy.beam
-      ~timeout:timeout_refine
-      ~beam_width
-      ~refine_degree
-      ~m0:(RInit, init_task_model)
-      ~data:(fun ?pred (r,m) -> data_of_model ?pred ~pruning:false r m)
-      ~code:(fun (r,m) (prs,dsri,dsro,dl_triples,lmd) -> lmd)
-      ~refinements:(fun (r,m) (prs,dsri,dsro,dl_triples,lmd) dl ->
-        log_refining r m prs dl;
-        Printf.printf "--- %dM memory used ---\n" ((Gc.quick_stat()).Gc.heap_words * 8 / 1000000);
-        Common.prof "Learning.task_refinements" (fun () ->
-            task_refinements m prs dsri dsro)) in
-  match lm_refine with
-  | [] -> assert false
-  | ((_,m_refine), (psr_refine,_,_,_,_), _)::_ ->
- *)   
-     let lm_prune, timed_out_prune =
-       if timeout_prune = 0 (* no pruning *)
-       then lm_refine, timed_out_refine
-       else (
-         print_endline "PRUNING PHASE";
-         Mdl.Strategy.beam
-           ~timeout:timeout_prune
-           ~beam_width:1
-           ~refine_degree
-           ~m0:(RInit, state_refine.m)
-           ~data:(fun ?pred (r,m) -> data_of_model ?pred ~pruning:true r m)
-           ~code:(fun (r,m) state -> state.lmd)
-           (* only parse ranks counted for input grids *)
-           ~refinements:(fun (r,m) state dl ->
-             log_refining state.r state.m state.prs dl;
-             Common.prof "Learning.task_prunings" (fun () ->
-                 task_prunings state.m state.drsi))
-       ) in
-     match lm_prune with
-     | [] -> assert false
-     | ((_,m_prune), state_prune, _)::_ ->
-        let result_pruning =
-          { task_model = m_prune;
-            pairs_reads = state_prune.prs;
-            timed_out = timed_out_prune;
-            memed_out = false } in
-        { result_refining;
-          result_pruning })
- *)
